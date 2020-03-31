8f4b3af99f03e764f3d5583b528ce004ceb0614c
Jesse Walling <jessephilipwalling@gmail.com>
2020-03-31 02:46:21 -0700

    insert basic sortable table with static values for study buddies

--- src/Course.js
+++ src/Course.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModules } from '@ag-grid-community/all-modules';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 class Course extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    modules: AllCommunityModules,
+	    columnDefs: [{
+		headerName: "Phone", field: "phone"
+	    }, {
+		headerName: "Grade", field: "grade", filter: 'agNumberColumnFilter'
+	    }, {
+		headerName: "User", field: "user"
+	    }],
+	    rowData: [{
+		phone: "+1 (206) 979 3532", grade: 3.5, user: "jpwall"
+	    }, {
+		phone: "+1 (206) 923 1037", grade: 3.0, user: "JPW"
+	    }, {
+		phone: "+1 (206) 979 3438", grade: 2.5, user: "jDoe"
+	    }]
+	}
+    }
+    
     render(){
 	const { params } = this.props.match;
 	return (
-	    <div>
-		<h2>Test</h2>
-		<p>{params.name}</p>
-	    </div>
+	    <div className="ag-theme-material" style={ {height: '100vh', width: '100vw'} }>
+		<AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}
+		></AgGridReact>
+		</div>
 	);
     }
 }
