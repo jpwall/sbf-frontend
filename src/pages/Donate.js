3bac236b9978c3818f73037d1b14e425d0ef167c
Jesse Walling <jessephilipwalling@gmail.com>
2020-06-16 23:54:17 -0700

    updated page description and changed wording for donate page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -23,7 +23,7 @@ class Donate extends Component {
                   <p>BTC (Bitcoin): 1NzBYg1jyLJFVmvWTGvQ5XhmTqzTYi4oj</p>
 
                   <h2>How will my donation be spent?</h2>
-                  <p>First, it will be used to cover the $0.05 phone verification fee per user. Next, it will be applied to server and hosting costs. As it currently stands, donations are not being used for profit. Your donation will be allocated to anything related to development and maintenance of STUDIUS.</p>
+                  <p>Your donation will be used to cover server, hosting, and phone verification costs. As it currently stands, donations are not being used for profit. Your donation will be allocated to anything related to development and maintenance of STUDIUS.</p>
                   <p>Any Bitcoin donations will be held and withdrawn from as necessary when dictated depending on the state of the market for the given purposes above.</p>
                 </div>
               </div>
