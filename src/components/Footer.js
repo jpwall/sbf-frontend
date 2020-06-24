d27d43d71fa8a8fdbc3cb404bd541c3910faf2e7
Jesse Walling <jessephilipwalling@gmail.com>
2020-06-23 22:54:59 -0700

    added feedback survey to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,8 @@ class Footer extends Component {
         return (
             <footer>
               <div><a href="/about">About and FAQ</a>|</div>
-              <div><a href="mailto:jessephilipwalling@gmail.com?subject=STUDIUS support / feedback:" target="_blank" rel="noopener noreferrer">Support and Feedback</a>|</div>
+              <div><a href="mailto:jessephilipwalling@gmail.com?subject=STUDIUS support:" target="_blank" rel="noopener noreferrer">Support</a>|</div>
+              <div><a href="https://forms.gle/uUGDfmDKthyWFDiV9" target="_blank" rel="noopener noreferrer">Feedback</a>|</div>
               <div><a href="/donate">Donate</a></div>
             </footer>
         );
